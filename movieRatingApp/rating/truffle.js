c813568e255e131fd447a30000360b2593dab5a2
Braulio Cesar Holtz Ribeiro <braulio.holtz@gmail.com>
2018-04-25 18:52:28 -0300

    first commit

---
+++ movieRatingApp/rating/truffle.js
@@ -0,0 +1,11 @@
+module.exports = {
+  networks:{
+    development:{
+      host:"localhost",
+      port:8546,
+      network_id:'*'
+    }
+  }
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+};
